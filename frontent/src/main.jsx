ea98ad7df930630f5b9fee573b2f64c566f6a60c
Aditya Pandey <honeypandey409@gmail.com>
2026-06-15 22:39:03 +0530

    feat: add helper / config file main.jsx

---
+++ frontent/src/main.jsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { AppProvider } from './context/AppContext.jsx';
+
+/* Modified by Antigravity: Wrapped App in AppProvider */
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </StrictMode>
+);
